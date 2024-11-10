5931b5cc7dbe521dd4c0a09f61eebff10e790a63
karinafs-yo <karinafs@upi.edu>
2024-11-10 16:06:58 +0700

    Menambahkan variabel sekolah di file latihan.py

--- latihan.py
+++ latihan.py
@@ -3,5 +3,6 @@ NIM 2402838
 KELAS RPL A"""
 nama = input("nama anda: ")
 umur = input("umur anda: ")
-print (f"Hallo, {nama} umur kamu adalah {umur}")
+sekolah = input("Nama sekolah: ")
+print (f"Hallo, {nama} umur kamu adalah {umur} dan sedang belajar di {sekolah}")
 
